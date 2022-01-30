caa7ce7813d4ff1f6bd76174c2e86319014df264
Denys <SenekaGl@gmail.com>
2022-01-30 21:43:06 +0200

    Update ALLOWED_HOSTS with site and development server URL

--- Matrix/settings.py
+++ Matrix/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-9mk)471m_(abl*
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hievo.herokuapp.com','127.0.0.1']
 
 
 # Application definition
